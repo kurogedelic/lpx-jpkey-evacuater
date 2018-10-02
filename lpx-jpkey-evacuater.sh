15b64155f7311550678e4014182915c99f949713
kurogedelic <kurogedelic@gmail.com>
2018-10-02 17:54:09 +0900

    日本語にしました

--- lpx-jpkey-evacuater.sh
+++ lpx-jpkey-evacuater.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 echo "+LPX Japanese Keycommand evacuater"
-echo "+Are you EVACUATE Japanese.logikcs? (Y/N)"
+echo "+Japanese.logikcsを退避させますか? (Y/N)"
 read ANSWER
+
 case $ANSWER in
     "" | "Y" | "y" | "yes" | "Yes" | "YES" ) mv "/Applications/Logic Pro X.app/Contents/Resources/Key Commands/Japanese.logikcs" "/Applications/Logic Pro X.app/Contents/Resources/Key Commands/Japanese.logikcs.bak"
     echo "Done.";;
     * ) break ;;
 esac
-echo "+Are you RECALL Japanese.logikcs? (Y/N)"
+
+echo "+Japanese.logikcsを呼び戻しますか? (Y/N)"
 read ANSWER
 case $ANSWER in
     "" | "Y" | "y" | "yes" | "Yes" | "YES" ) mv "/Applications/Logic Pro X.app/Contents/Resources/Key Commands/Japanese.logikcs.bak" "/Applications/Logic Pro X.app/Contents/Resources/Key Commands/Japanese.logikcs"
